7b57a9756c500d79b474e77a1f665ece3f69d78e
SARATH <SARATH@DESKTOP-TC9F67T>
2021-01-06 12:31:24 +0530

    WP-5520-THIRD UPDATE FILE

---
+++ src/main/java/com/example/demo/SpringbootCsvApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCsvApplication.class, args);
+	}
+
+}
